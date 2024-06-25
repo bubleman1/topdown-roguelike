3b2232ceb97ed0eb36b8268f507113544aa2bda4
FelisTonchev <Felix1tonchev1@gmail.com>
2024-06-25 14:41:23 +0300

    Mid Update
    Fullscreen Button in OptionsMenu(work)
    PauseMenu in MainGame(work 90%)
    1 bug

--- Scripts/options_main_menu.gd
+++ Scripts/options_main_menu.gd
@@ -2,3 +2,9 @@ extends Control
 
 func _on_back_pressed():
 	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
+	
+func _on_full_screen_button_toggled(button_pressed):
+	if button_pressed == true:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
