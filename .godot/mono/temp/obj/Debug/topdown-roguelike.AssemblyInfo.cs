67c6ff2263fa503f08ec4e6553efe8a4c78f4f04
FelisTonchev <Felix1tonchev1@gmail.com>
2024-07-09 14:30:30 +0300

    p

--- .godot/mono/temp/obj/Debug/topdown-roguelike.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/topdown-roguelike.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("topdown-roguelike")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb8437a657e24b181e550b137891f2c20efeaa03")]
 [assembly: System.Reflection.AssemblyProductAttribute("topdown-roguelike")]
 [assembly: System.Reflection.AssemblyTitleAttribute("topdown-roguelike")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
