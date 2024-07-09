1cfee3421ef2f13fa606186d9189e7a2d5f77b96
mumugrig <mumugrig@gmail.com>
2024-07-09 16:39:12 +0300

    snake animaciq

--- Scripts/Enemies/Snake.cs
+++ Scripts/Enemies/Snake.cs
@@ -9,6 +9,8 @@ public partial class Snake : CharacterBody2D
 	public HurtboxComponent hurtboxComponent {get;set;}
 	[Export]
 	public DetectionComponent detectionComponent {get;set;}
+	[Export]
+	public AnimatedSprite2D animatedSprite {get;set;}
 	
 	public override void _Ready(){
 		hurtboxComponent.Dead+= OnDead;
@@ -17,13 +19,21 @@ public partial class Snake : CharacterBody2D
 	public override void _Process(double delta){
 		velocityComponent.Velocity = Vector2.Zero;
 		if(detectionComponent.Player!=null){
+			animatedSprite.Play("Snake Move");
 			velocityComponent.GoTo(detectionComponent.Player.Position);
 		}
 		else
 		{
 			velocityComponent.Stop();
+			animatedSprite.Play("Snake Idle");
 		}
 		velocityComponent.ProcessVelocity();
+		if(velocityComponent.Velocity.X - velocityComponent.Knockback.X > 0){
+			animatedSprite.FlipH = false;
+		}
+		else if(velocityComponent.Velocity.X - velocityComponent.Knockback.X < 0){
+			animatedSprite.FlipH = true;
+		}
 		velocityComponent.ProcessKnockback();
 		GD.Print(velocityComponent.Knockback);
 		MoveAndCollide(velocityComponent.Velocity * (float)delta);
